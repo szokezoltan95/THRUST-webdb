5d195d7cef90d25f8dd7fb1f61bf6072113b2011
szokezoltan95 <67110675+szokezoltan95@users.noreply.github.com>
2026-09-23 09:41:08 +0200

    feat: include student API router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
-from app.api import admin, auth, public
+from app.api import admin, auth, public, student
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_name, docs_url=None, redoc_url=None)
@@ -13,6 +13,7 @@ app.add_middleware(
 app.include_router(public.router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
 app.include_router(admin.router, prefix="/api")
+app.include_router(student.router, prefix="/api")
 
 
 @app.get("/api/health")
